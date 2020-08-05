550f51ab48d1078bb9570f36b884b38f3884959e
Daniel Collins <dpcollins@google.com>
2020-08-05 12:47:36 -0400

    feat: Implement DefaultRouingPolicy

---
+++ google/cloud/pubsublite/internal/wire/routing_policy.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from google.cloud.pubsublite.partition import Partition
+from google.cloud.pubsublite_v1.types.common import PubSubMessage
+
+
+class RoutingPolicy(ABC):
+  @abstractmethod
+  def route(self, message: PubSubMessage) -> Partition:
+    """
+    Route a message to a given partition.
+    Args:
+      message: The message to route
+
+    Returns: The partition to route to
+
+    """
+    raise NotImplementedError()
