f72a2f0b8452b4598ae901073cfc06ca017079fe
dpcollins-google <40498610+dpcollins-google@users.noreply.github.com>
2020-08-10 10:56:27 -0400

    feat: Implement RoutingPolicy (#5)
    * feat: Implement python retrying connection, which generically retries stream errors.
    
    * fix: Add asynctest to tests_require.
    
    * feat: Implement DefaultRouingPolicy
    
    * fix: Add class comments.
    
    * feat: Implement python retrying connection, which generically retries stream errors (#4)
    
    * feat: Implement python retrying connection, which generically retries stream errors.
    
    * fix: Add asynctest to tests_require.
    
    * fix: Add class comments.
    
    * feat: Implement DefaultRouingPolicy
    
    * fix: Add class comments.
    
    * docs: Add comment to DefaultRoutingPolicy.

---
+++ google/cloud/pubsublite/internal/wire/routing_policy.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+from google.cloud.pubsublite.partition import Partition
+from google.cloud.pubsublite_v1.types.common import PubSubMessage
+
+
+class RoutingPolicy(ABC):
+  """A policy for how to route messages."""
+  @abstractmethod
+  def route(self, message: PubSubMessage) -> Partition:
+    """
+    Route a message to a given partition.
+    Args:
+      message: The message to route
+
+    Returns: The partition to route to
+
+    """
+    raise NotImplementedError()
