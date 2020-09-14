9901c748b8fc785a790f985b2681ba7f42a5810a
dpcollins-google <40498610+dpcollins-google@users.noreply.github.com>
2020-09-14 09:38:18 -0400

    Update committer.py

--- google/cloud/pubsublite/internal/wire/committer.py
+++ google/cloud/pubsublite/internal/wire/committer.py
@@ -6,7 +6,7 @@ from google.cloud.pubsublite_v1 import Cursor
 
 class Committer(AsyncContextManager):
   """
-  A Committer is able to commit subscribers completed offsets.
+  A Committer is able to commit subscribers' completed offsets.
   """
   @abstractmethod
   async def commit(self, cursor: Cursor) -> None:
