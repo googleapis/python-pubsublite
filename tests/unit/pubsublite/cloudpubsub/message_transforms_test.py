c432d1dac86aaba62303c33c7277279790d060e8
Daniel Collins <dpcollins@google.com>
2020-09-22 09:49:20 -0400

    fix: Replace try-blocks with pytest.raises.

--- tests/unit/pubsublite/cloudpubsub/message_transforms_test.py
+++ tests/unit/pubsublite/cloudpubsub/message_transforms_test.py
@@ -1,9 +1,10 @@
 import datetime
 
-import grpc
-from google.api_core.exceptions import GoogleAPICallError
+import pytest
+from google.api_core.exceptions import InvalidArgument
 from google.protobuf.timestamp_pb2 import Timestamp
 from google.pubsub_v1 import PubsubMessage
+
 from google.cloud.pubsublite.cloudpubsub.message_transforms import PUBSUB_LITE_EVENT_TIME, to_cps_subscribe_message, \
   encode_attribute_event_time, from_cps_publish_message
 from google.cloud.pubsublite_v1 import SequencedMessage, Cursor, PubSubMessage, AttributeValues
@@ -12,43 +13,31 @@ NOT_UTF8 = bytes.fromhex('ffff')
 
 
 def test_invalid_subscribe_transform_key():
-  try:
+  with pytest.raises(InvalidArgument):
     to_cps_subscribe_message(
       SequencedMessage(message=PubSubMessage(key=NOT_UTF8), publish_time=Timestamp(), cursor=Cursor(offset=10),
                        size_bytes=10))
-    assert False
-  except GoogleAPICallError as e:
-    assert e.grpc_status_code == grpc.StatusCode.INVALID_ARGUMENT
 
 
 def test_invalid_subscribe_contains_magic_attribute():
-  try:
+  with pytest.raises(InvalidArgument):
     to_cps_subscribe_message(SequencedMessage(
       message=PubSubMessage(key=b'def', attributes={PUBSUB_LITE_EVENT_TIME: AttributeValues(values=[b'abc'])}),
       publish_time=Timestamp(seconds=10), cursor=Cursor(offset=10), size_bytes=10))
-    assert False
-  except GoogleAPICallError as e:
-    assert e.grpc_status_code == grpc.StatusCode.INVALID_ARGUMENT
 
 
 def test_invalid_subscribe_contains_multiple_attributes():
-  try:
+  with pytest.raises(InvalidArgument):
     to_cps_subscribe_message(SequencedMessage(
       message=PubSubMessage(key=b'def', attributes={'xyz': AttributeValues(values=[b'abc', b''])}),
       publish_time=Timestamp(seconds=10), cursor=Cursor(offset=10), size_bytes=10))
-    assert False
-  except GoogleAPICallError as e:
-    assert e.grpc_status_code == grpc.StatusCode.INVALID_ARGUMENT
 
 
 def test_invalid_subscribe_contains_non_utf8_attributes():
-  try:
+  with pytest.raises(InvalidArgument):
     to_cps_subscribe_message(SequencedMessage(
       message=PubSubMessage(key=b'def', attributes={'xyz': AttributeValues(values=[NOT_UTF8])}),
       publish_time=Timestamp(seconds=10), cursor=Cursor(offset=10), size_bytes=10))
-    assert False
-  except GoogleAPICallError as e:
-    assert e.grpc_status_code == grpc.StatusCode.INVALID_ARGUMENT
 
 
 def test_subscribe_transform_correct():
@@ -65,11 +54,8 @@ def test_subscribe_transform_correct():
 
 
 def test_publish_invalid_event_time():
-  try:
+  with pytest.raises(InvalidArgument):
     from_cps_publish_message(PubsubMessage(attributes={PUBSUB_LITE_EVENT_TIME: 'probably not an encoded proto'}))
-    assert False
-  except GoogleAPICallError as e:
-    assert e.grpc_status_code == grpc.StatusCode.INVALID_ARGUMENT
 
 
 def test_publish_valid_transform():
