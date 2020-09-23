eac5b048a7c5a59e287238fe7a55d1a81d498289
Daniel Collins <dpcollins@google.com>
2020-09-23 16:58:56 -0400

    Implement CPS subscriber that takes a message callback and returns a StreamingPullFuture.

---
+++ google/cloud/pubsublite/cloudpubsub/internal/streaming_pull_manager.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+from typing import Optional, Callable
+
+from google.api_core.exceptions import GoogleAPICallError
+
+
+CloseCallback = Callable[["StreamingPullManager", Optional[GoogleAPICallError]], None]
+
+
+class StreamingPullManager(ABC):
+  """The API expected by StreamingPullFuture."""
+  @abstractmethod
+  def add_close_callback(self, close_callback: CloseCallback):
+    pass
+
+  @abstractmethod
+  def close(self):
+    pass
