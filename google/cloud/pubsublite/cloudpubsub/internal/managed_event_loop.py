58fda6fb7041cd05b7ac6fb8915e1588d2200651
hannahrogers-google <52459909+hannahrogers-google@users.noreply.github.com>
2020-09-22 21:53:18 -0400

    feat: Implement Publisher API (#21)
    * feat: Implement transforms to/from Pub/Sub messages and Pub/Sub Lite messages.
    
    * feat: Implement Pub/Sub Publisher
    
    * fix: Re-delete b64_utils after merge.
    
    Co-authored-by: Daniel Collins <dpcollins@google.com>

---
+++ google/cloud/pubsublite/cloudpubsub/internal/managed_event_loop.py
@@ -0,0 +1,23 @@
+from asyncio import AbstractEventLoop, new_event_loop, run_coroutine_threadsafe
+from concurrent.futures import Future
+from threading import Thread
+from typing import ContextManager
+
+
+class ManagedEventLoop(ContextManager):
+  _loop: AbstractEventLoop
+  _thread: Thread
+
+  def __init__(self):
+    self._loop = new_event_loop()
+    self._thread = Thread(target=lambda: self._loop.run_forever())
+
+  def __enter__(self):
+    self._thread.start()
+
+  def __exit__(self, __exc_type, __exc_value, __traceback):
+    self._loop.call_soon_threadsafe(self._loop.stop)
+    self._thread.join()
+
+  def submit(self, coro) -> Future:
+    return run_coroutine_threadsafe(coro, self._loop)
---
+++ google/cloud/pubsublite/cloudpubsub/publisher.py
@@ -0,0 +1,50 @@
+from abc import abstractmethod
+from typing import AsyncContextManager, Mapping, ContextManager
+from concurrent import futures
+
+
+class AsyncPublisher(AsyncContextManager):
+  """
+  An AsyncPublisher publishes messages similar to Google Pub/Sub, but must be used in an
+  async context. Any publish failures are permanent.
+  """
+
+  @abstractmethod
+  async def publish(self, data: bytes, ordering_key: str = "", **attrs: Mapping[str, str]) -> str:
+    """
+    Publish a message.
+
+    Args:
+      data: The bytestring payload of the message
+      ordering_key: The key to enforce ordering on, or "" for no ordering.
+      **attrs: Additional attributes to send.
+
+    Returns:
+      An ack id, which can be decoded using PublishMetadata.decode.
+
+    Raises:
+      GoogleApiCallError: On a permanent failure.
+    """
+
+
+class Publisher(ContextManager):
+  """
+  A Publisher publishes messages similar to Google Pub/Sub. Any publish failures are permanent.
+  """
+
+  @abstractmethod
+  def publish(self, data: bytes, ordering_key: str = "", **attrs: Mapping[str, str]) -> 'futures.Future[str]':
+    """
+    Publish a message.
+
+    Args:
+      data: The bytestring payload of the message
+      ordering_key: The key to enforce ordering on, or "" for no ordering.
+      **attrs: Additional attributes to send.
+
+    Returns:
+      A future completed with an ack id, which can be decoded using PublishMetadata.decode.
+
+    Raises:
+      GoogleApiCallError: On a permanent failure.
+    """
