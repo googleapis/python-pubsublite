9fa4c3726e145308b691923e8bb336b84907679d
Daniel Collins <dpcollins@google.com>
2020-09-22 22:01:36 -0400

    feat: Add DefaultNackHandler.

--- google/cloud/pubsublite/cloudpubsub/nack_handler.py
+++ google/cloud/pubsublite/cloudpubsub/nack_handler.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from typing import Callable
 
+from google.api_core.exceptions import FailedPrecondition
 from google.pubsub_v1 import PubsubMessage
 
 
@@ -21,3 +22,10 @@ class NackHandler(ABC):
       GoogleAPICallError: To fail the client if raised inline.
     """
     pass
+
+
+class DefaultNackHandler(NackHandler):
+  def on_nack(self, message: PubsubMessage, ack: Callable[[], None]):
+    raise FailedPrecondition(
+      "You may not nack messages by default when using a PubSub Lite client. See NackHandler for how to customize"
+      " this.")
