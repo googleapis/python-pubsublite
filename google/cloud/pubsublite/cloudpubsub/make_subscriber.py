7bf69c1ce3e16f3c5caec7fd9d03b3d631a4cce7
Daniel Collins <dpcollins@google.com>
2020-09-23 16:58:56 -0400

    Implement CPS subscriber that takes a message callback and returns a StreamingPullFuture.

--- google/cloud/pubsublite/cloudpubsub/make_subscriber.py
+++ google/cloud/pubsublite/cloudpubsub/make_subscriber.py
@@ -1,17 +1,19 @@
+from concurrent.futures.thread import ThreadPoolExecutor
 from typing import Optional, Mapping, Set, AsyncIterator
 from uuid import uuid4
 
 from google.api_core.client_options import ClientOptions
 from google.auth.credentials import Credentials
-
+from google.cloud.pubsub_v1.subscriber.futures import StreamingPullFuture
 from google.cloud.pubsublite.cloudpubsub.flow_control_settings import FlowControlSettings
 from google.cloud.pubsublite.cloudpubsub.internal.ack_set_tracker_impl import AckSetTrackerImpl
 from google.cloud.pubsublite.cloudpubsub.internal.assigning_subscriber import PartitionSubscriberFactory, \
   AssigningSubscriber
 from google.cloud.pubsublite.cloudpubsub.internal.single_partition_subscriber import SinglePartitionSubscriber
+import google.cloud.pubsublite.cloudpubsub.internal.subscriber_impl as cps_subscriber
 from google.cloud.pubsublite.cloudpubsub.message_transformer import MessageTransformer, DefaultMessageTransformer
 from google.cloud.pubsublite.cloudpubsub.nack_handler import NackHandler, DefaultNackHandler
-from google.cloud.pubsublite.cloudpubsub.subscriber import AsyncSubscriber
+from google.cloud.pubsublite.cloudpubsub.subscriber import AsyncSubscriber, MessageCallback
 from google.cloud.pubsublite.endpoints import regional_endpoint
 from google.cloud.pubsublite.internal.wire.assigner import Assigner
 from google.cloud.pubsublite.internal.wire.assigner_impl import AssignerImpl
@@ -20,7 +22,7 @@ from google.cloud.pubsublite.internal.wire.fixed_set_assigner import FixedSetAss
 from google.cloud.pubsublite.internal.wire.gapic_connection import GapicConnectionFactory
 from google.cloud.pubsublite.internal.wire.merge_metadata import merge_metadata
 from google.cloud.pubsublite.internal.wire.pubsub_context import pubsub_context
-from google.cloud.pubsublite.internal.wire.subscriber_impl import SubscriberImpl
+import google.cloud.pubsublite.internal.wire.subscriber_impl as wire_subscriber
 from google.cloud.pubsublite.partition import Partition
 from google.cloud.pubsublite.paths import SubscriptionPath
 from google.cloud.pubsublite.routing_metadata import subscription_routing_metadata
@@ -63,14 +65,14 @@ def _make_partition_subscriber_factory(
     def cursor_connection_factory(requests: AsyncIterator[StreamingCommitCursorRequest]):
       return cursor_client.streaming_commit_cursor(requests, metadata=list(final_metadata.items()))
 
-    wire_subscriber = SubscriberImpl(
+    subscriber = wire_subscriber.SubscriberImpl(
       InitialSubscribeRequest(subscription=str(subscription), partition=partition.value),
       _DEFAULT_FLUSH_SECONDS, GapicConnectionFactory(subscribe_connection_factory))
     committer = CommitterImpl(
       InitialCommitCursorRequest(subscription=str(subscription), partition=partition.value),
       _DEFAULT_FLUSH_SECONDS, GapicConnectionFactory(cursor_connection_factory))
     ack_set_tracker = AckSetTrackerImpl(committer)
-    return SinglePartitionSubscriber(wire_subscriber, flow_control_settings, ack_set_tracker, nack_handler,
+    return SinglePartitionSubscriber(subscriber, flow_control_settings, ack_set_tracker, nack_handler,
                                      message_transformer)
 
   return factory
@@ -124,3 +126,46 @@ def make_async_subscriber(
                                                                     metadata, per_partition_flow_control_settings,
                                                                     nack_handler, message_transformer)
   return AssigningSubscriber(assigner, partition_subscriber_factory)
+
+
+def make_subscriber(
+    subscription: SubscriptionPath,
+    per_partition_flow_control_settings: FlowControlSettings,
+    callback: MessageCallback,
+    nack_handler: Optional[NackHandler] = None,
+    message_transformer: Optional[MessageTransformer] = None,
+    fixed_partitions: Optional[Set[Partition]] = None,
+    executor: Optional[ThreadPoolExecutor] = None,
+    credentials: Optional[Credentials] = None,
+    client_options: Optional[ClientOptions] = None,
+    metadata: Optional[Mapping[str, str]] = None) -> StreamingPullFuture:
+  """
+  Make a Pub/Sub Lite Subscriber.
+
+  Args:
+    subscription: The subscription to subscribe to.
+    per_partition_flow_control_settings: The flow control settings for each partition subscribed to. Note that these
+      settings apply to each partition individually, not in aggregate.
+    callback: The callback to call with each message.
+    nack_handler: An optional handler for when nack() is called on a Message. The default will fail the client.
+    message_transformer: An optional transformer from Pub/Sub Lite messages to Cloud Pub/Sub messages.
+    fixed_partitions: A fixed set of partitions to subscribe to. If not present, will instead use auto-assignment.
+    executor: The executor to use for user callbacks. If not provided, will use the default constructed
+      ThreadPoolExecutor. If provided a single threaded executor, messages will be ordered per-partition, but take care
+      that the callback does not block for too long as it will impede forward progress on all partitions.
+    credentials: The credentials to use to connect. GOOGLE_DEFAULT_CREDENTIALS is used if None.
+    client_options: Other options to pass to the client. Note that if you pass any you must set api_endpoint.
+    metadata: Additional metadata to send with the RPC.
+
+  Returns:
+    A StreamingPullFuture, managing the subscriber's lifetime.
+  """
+  underlying = make_async_subscriber(
+    subscription, per_partition_flow_control_settings, nack_handler, message_transformer, fixed_partitions, credentials,
+    client_options, metadata)
+  if executor is None:
+    executor = ThreadPoolExecutor()
+  subscriber = cps_subscriber.SubscriberImpl(underlying, callback, executor)
+  future = StreamingPullFuture(subscriber)
+  subscriber.__enter__()
+  return future
