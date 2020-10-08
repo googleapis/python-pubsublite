f4f71488e9bfa5e14bbfb61e6ce0d95fa3b19b10
Tianzi Cai <tianzi@google.com>
2020-10-07 17:45:08 -0700

    pwd

--- samples-test-script.sh
+++ samples-test-script.sh
@@ -1,8 +1,9 @@
-export GOOGLE_CLOUD_PROJECT_NUMBER=502009289245
-export INSTALL_LIBRARY_FROM_SOURCE=True
+pwd
+# export GOOGLE_CLOUD_PROJECT_NUMBER=502009289245
+# export INSTALL_LIBRARY_FROM_SOURCE=True
 
-cd ./samples/snippets
+# cd ./samples/snippets
 
-python3.6 -m pip install --quiet nox-automation
-python3.6 -m pip install --upgrade --quiet nox
-python3.6 -m nox
+# python3.6 -m pip install --quiet nox-automation
+# python3.6 -m pip install --upgrade --quiet nox
+# python3.6 -m nox
