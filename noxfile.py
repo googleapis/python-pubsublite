8c2ded1d6beff3bb181eddba83a1ee79a6ca362e
Tianzi Cai <tianzi@google.com>
2020-11-13 10:27:46 -0800

    add asynctest as a test dep

--- noxfile.py
+++ noxfile.py
@@ -70,7 +70,7 @@ def lint_setup_py(session):
 
 def default(session):
     # Install all test dependencies, then install this package in-place.
-    session.install("asyncmock", "pytest-asyncio")
+    session.install("asyncmock", "pytest-asyncio", "asynctest")
 
     session.install(
         "mock", "pytest", "pytest-cov",
