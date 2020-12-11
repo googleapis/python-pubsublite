0bd8fc25097f717e87d973dab521ab9e8d4243e9
dpcollins-google <40498610+dpcollins-google@users.noreply.github.com>
2020-12-11 12:57:16 -0500

    Update endpoints.py to explicitly specify protocol

--- google/cloud/pubsublite/internal/endpoints.py
+++ google/cloud/pubsublite/internal/endpoints.py
@@ -16,4 +16,4 @@ from google.cloud.pubsublite.types import CloudRegion
 
 
 def regional_endpoint(region: CloudRegion):
-    return f"{region}-pubsublite.googleapis.com"
+    return f"dns://{region}-pubsublite.googleapis.com"
