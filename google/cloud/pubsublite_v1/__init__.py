bed2af4bcaa2d90459a01d6b1afb6904f90de77e
yoshi-automation <yoshi-automation@google.com>
2021-03-09 13:48:24 -0800

    test(python): fix template defaults for noxfile default py versions
    * test(python): fix template defaults for noxfile default py versions
    
    * fix: firestore builds were having issue with 3.9 missing, default to 3.8
    
    * test: update test to match new default
    
    * test: for now use 3.8, as we may not have 3.9 images for all CI yet
    
    Source-Author: Christopher Wilcox <crwilcox@google.com>
    Source-Date: Thu Nov 12 17:27:23 2020 -0800
    Source-Repo: googleapis/synthtool
    Source-Sha: d5fc0bcf9ea9789c5b0e3154a9e3b29e5cea6116
    Source-Link: https://github.com/googleapis/synthtool/commit/d5fc0bcf9ea9789c5b0e3154a9e3b29e5cea6116

--- google/cloud/pubsublite_v1/__init__.py
+++ google/cloud/pubsublite_v1/__init__.py
@@ -77,6 +77,7 @@ from .types.topic_stats import ComputeMessageStatsResponse
 
 
 __all__ = (
+    "AdminServiceClient",
     "AttributeValues",
     "CommitCursorRequest",
     "CommitCursorResponse",
@@ -85,7 +86,6 @@ __all__ = (
     "CreateSubscriptionRequest",
     "CreateTopicRequest",
     "Cursor",
-    "CursorServiceClient",
     "DeleteSubscriptionRequest",
     "DeleteTopicRequest",
     "FlowControlRequest",
@@ -135,5 +135,5 @@ __all__ = (
     "TopicStatsServiceClient",
     "UpdateSubscriptionRequest",
     "UpdateTopicRequest",
-    "AdminServiceClient",
+    "CursorServiceClient",
 )
