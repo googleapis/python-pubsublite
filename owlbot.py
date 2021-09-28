83100f032dd7bc75f2b7b6129dea9876ccf680dc
Bu Sun Kim <busunkim@google.com>
2021-09-28 21:44:24 +0000

    chore: exclude noxfile.py from generated lib copy

--- owlbot.py
+++ owlbot.py
@@ -55,6 +55,7 @@ for library in s.get_staging_dirs(default_version):
         "README.rst",
         "scripts/fixup*.py",  # new libraries do not need the keyword fixup script
         "setup.py",
+        "noxfile.py"
     ]
     s.move(library, excludes=excludes)
 
