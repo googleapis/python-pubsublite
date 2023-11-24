0c6044b608c3b2d5ea86556f798f6ed37f9fe882
Anthonios Partheniou <partheniou@google.com>
2023-11-24 14:32:52 -0500

    Add python 3.12 to owlbot.py

--- owlbot.py
+++ owlbot.py
@@ -63,7 +63,7 @@ s.remove_staging_dirs()
 templated_files = gcp.CommonTemplates().py_library(
     cov_level=96,
     microgenerator=True,
-    unit_test_python_versions=["3.8", "3.9", "3.10", "3.11"],
+    unit_test_python_versions=["3.8", "3.9", "3.10", "3.11", "3.12"],
     versions=gcp.common.detect_versions(path="./google", default_first=True),
 )
 s.move(
